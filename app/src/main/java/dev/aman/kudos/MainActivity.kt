a59c4339fba11d8d632a442ed7c7c7d518dfc9a7
codecraft26 <codescraft26@gmail.com>
2022-03-08 00:43:42 +0530

    adding loading through glide

--- app/src/main/java/dev/aman/kudos/MainActivity.kt
+++ app/src/main/java/dev/aman/kudos/MainActivity.kt
@@ -1,14 +1,45 @@
 package dev.aman.kudos
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.media.Image
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
+import dev.aman.kudos.databinding.ActivityMainBinding
+import dev.aman.kudos.databinding.ActivitySigninBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var  binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        Glide.with(this).load(currentUser!!.photoUrl).into(binding.profileimg)
+
+
+
     }
 
-    fun onSignOutButtonClicked(view: View) {}
+    fun onSignOutButtonClicked(view: View) {
+        AuthUI.getInstance()
+            .signOut(this)
+            .addOnCompleteListener {
+                Toast.makeText(this,
+                    "signout success",
+                    Toast.LENGTH_SHORT
+                ).show()
+               val intent=Intent(this,SigninActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+
+    }
 }
